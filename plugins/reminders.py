c4af07031e2fc8bb529c0979cda282904471b1a0
Meitar Reihan <meitarr013@gmail.com>
2017-09-20 13:48:05 +0300

    finished conversation

--- plugins/reminders.py
+++ plugins/reminders.py
@@ -14,18 +14,21 @@ from telegram.ext import RegexHandler
 import tools
 
 MESSAGE, CONFIRM_MESSAGE, DATE, CUSTOM_DATE, HOUR, CONFIRM_TIME, REPEAT, CUSTOM_DAYS, CONFIRM_REPEAT = range(9)
+EVERY_DAY, EVERY_MONTH, CUSTOM = range(3)
 
-REMIND_CANCEL_MSG = '\n(Remember, if you regret, just use /cancel to cancel)'
-START_MSG = 'What should I remind you?\n'
-CONF_TEXT_MSG = 'Are you sure that what you want me to remind you is: "{0}"?\n'
-GET_DATE_MSG = 'tell me when\n'
-GET_C_DATE_MSG = 'Tell me exactly when \nwith a date as d.m.yyyy (exa: 1.1.2000)\nor with a day name (exa: sunday)'
-GET_HOUR_MSG = ''
-CONF_TIME_MSG = ''
-GET_REP_MSG = ''
-GET_C_REP_MSG = ''
-CONF_REP_MSG = ''
-END_MSG = ''
+REMIND_CANCEL_MSG = '\n(Remember, if you regret, just use /cancel to discard)\n'
+START_MSG = 'What do you need me to remind you of?\n'
+CONF_TEXT_MSG = 'Are you sure that you want to be reminded about "{0}"?\n'
+GET_DATE_MSG = 'Should i remind you of that today or tomorrow?\n'
+GET_C_DATE_MSG = 'Tell me on what day do you need to be reminded please, \n' \
+                 'with a date in this format - DD.MM.YYYY (ex: 1.1.2000),' \
+                 '\nor just tell me what day (ex: sunday)\n'
+GET_HOUR_MSG = 'What time do you need me to remind you of that? Please write in this format - HH:MM\n'
+CONF_TIME_MSG = 'So let\'s see if i got it right. You want me to remind you on {0} at {1}. Am I correct?\n'
+GET_REP_MSG = 'Do you need to be reminded of that repeatedly? For instance, every week?\n'
+GET_C_REP_MSG = 'Choose the days you want it to repeat.\n\nYou already choosed those days:\n{0}\n'
+CONF_REP_MSG = 'All right, you want me to remind you {0}. Correct?\n'
+END_MSG = 'Excellent! Your reminder has been set! \n'
 
 DAYS = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
 WEEK_LENGTH = 7
@@ -106,13 +109,13 @@ def is_message_good(bot, update, user_data):
     """
     message = update.message  # type: telegram.Message
     if message.text == "Yes!":
-        reply_keyboard = tools.build_menu(['Today', 'Tomorrow', 'Further'], 2)
+        reply_keyboard = tools.build_menu(['Today', 'Tomorrow', 'No, a different date'], 2)
 
-        message.reply_text('Good!\n\nNow ' + GET_DATE_MSG + REMIND_CANCEL_MSG,
+        message.reply_text('Good!\n\n' + GET_DATE_MSG + REMIND_CANCEL_MSG,
                            reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, selective=True))
         return DATE
     else:
-        message.reply_text('Well...\nlets fix it.\n\n' + START_MSG + REMIND_CANCEL_MSG,
+        message.reply_text('Well...\nlet\'s fix it.\n\n' + START_MSG + REMIND_CANCEL_MSG,
                            reply_markup=ForceReply(selective=True))
         return MESSAGE
 
@@ -136,10 +139,10 @@ def select_date_type(bot, update, user_data):
     elif message.text == "Tomorrow":
         data['date'] = (datetime.datetime.today() + datetime.timedelta(days=1)).strftime(DATE_FORMAT)
     else:
-        message.reply_text(GET_C_DATE_MSG,
+        message.reply_text(GET_C_DATE_MSG + REMIND_CANCEL_MSG,
                            reply_markup=ForceReply(selective=True))
         return CUSTOM_DATE
-    message.reply_text("the user_data: " + str(user_data))
+    message.reply_text(GET_HOUR_MSG + REMIND_CANCEL_MSG)
     return HOUR
 
 
@@ -157,7 +160,8 @@ def handle_custom_date(bot, update, user_data):
     message = update.message  # type: telegram.Message
     data = user_data['remind']  # type: dict
     data['date'] = message.text
-    message.reply_text("the user_data: " + str(user_data))
+    message.reply_text(GET_HOUR_MSG + REMIND_CANCEL_MSG)
+    return HOUR
 
 
 def handle_day_name(bot, update, user_data):
@@ -174,7 +178,161 @@ def handle_day_name(bot, update, user_data):
     message = update.message  # type: telegram.Message
     data = user_data['remind']  # type: dict
     data['date'] = get_future_weekday_date(message.text).strftime(DATE_FORMAT)
-    message.reply_text("the user_data: " + str(user_data))
+    message.reply_text(GET_HOUR_MSG + REMIND_CANCEL_MSG)
+    return HOUR
+
+
+def get_hour(bot, update, user_data):
+    """
+    ***----***
+    :param bot: the bot class
+    :type bot: telegram.Bot
+    :param update: the update message
+    :type update: telegram.Update
+    :param user_data: the user's data
+    :type user_data: dict
+    :return:
+    """
+    message = update.message  # type: telegram.Message
+    data = user_data['remind']  # type: dict
+
+    hour = str(message.text)
+
+    if data['date'] == datetime.datetime.today().strftime(DATE_FORMAT):
+        spited_hour = hour.split(':')
+        if int(spited_hour[0]) < datetime.datetime.now().hour or \
+                (int(spited_hour[0]) == datetime.datetime.now().hour and
+                 int(spited_hour[1]) <= datetime.datetime.now().minute):
+            message.reply_text("That hour already passed...\nFix it.\n\n" + GET_HOUR_MSG)
+            return HOUR
+
+    data['hour'] = hour
+    reply_keyboard = [['Yes!', 'No!']]
+
+    message.reply_text(CONF_TIME_MSG.format(data['date'], data['hour']) + REMIND_CANCEL_MSG,
+                       reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, selective=True))
+    return CONFIRM_TIME
+
+
+def is_time_good(bot, update, user_data):
+    """
+    ***----***
+    :param bot: the bot class
+    :type bot: telegram.Bot
+    :param update: the update message
+    :type update: telegram.Update
+    :param user_data: the user's data
+    :type user_data: dict
+    :return:
+    """
+    message = update.message  # type: telegram.Message
+    if message.text == "Yes!":
+        reply_keyboard = tools.build_menu(['Every day', 'Every month', 'Custom', 'Nope'], 3)
+
+        message.reply_text('Good!\n\nNow ' + GET_REP_MSG + REMIND_CANCEL_MSG,
+                           reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, selective=True))
+        return REPEAT
+    else:
+        reply_keyboard = tools.build_menu(['Today', 'Tomorrow', 'No, a different date'], 2)
+
+        message.reply_text('Well...\nlet\'s fix it.\n\n' + GET_DATE_MSG + REMIND_CANCEL_MSG,
+                           reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, selective=True))
+        return DATE
+
+
+def get_repeat_type(bot, update, user_data):
+    """
+    ***----***
+    :param bot: the bot class
+    :type bot: telegram.Bot
+    :param update: the update message
+    :type update: telegram.Update
+    :param user_data: the user's data
+    :type user_data: dict
+    :return:
+    """
+    message = update.message  # type: telegram.Message
+    data = user_data['remind']  # type: dict
+
+    reply_keyboard = [['Yes!', 'No!']]
+
+    if message.text == 'Every day':
+        data['repeat'] = EVERY_DAY
+    elif message.text == 'Every month':
+        data['repeat'] = EVERY_MONTH
+    elif message.text == 'Custom':
+        reply_keyboard = tools.build_menu(DAYS, 3)
+
+        data['repeat'] = CUSTOM
+        data['custom_repeat'] = []
+        message.reply_text(GET_C_REP_MSG.format('None'),
+                           reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, selective=True))
+        return CUSTOM_DAYS
+    else:
+        data['repeat'] = None
+        message.reply_text(CONF_REP_MSG.format("with no repeat") + REMIND_CANCEL_MSG,
+                           reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, selective=True))
+        return CONFIRM_REPEAT
+
+    message.reply_text(CONF_REP_MSG.format(message.text.lower()) + REMIND_CANCEL_MSG,
+                       reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, selective=True))
+    return CONFIRM_REPEAT
+
+
+def handle_custom_days(bot, update, user_data):
+    """
+    ***----***
+    :param bot: the bot class
+    :type bot: telegram.Bot
+    :param update: the update message
+    :type update: telegram.Update
+    :param user_data: the user's data
+    :type user_data: dict
+    :return:
+    """
+    message = update.message  # type: telegram.Message
+    data = user_data['remind']  # type: dict
+    chosen_days = data['custom_repeat']  # type: list
+
+    if message.text == 'Done':
+        reply_keyboard = [['Yes!', 'No!']]
+        message.reply_text(CONF_REP_MSG.format('every ' + ', '.join([DAYS[i] for i in chosen_days])),
+                           reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, selective=True))
+        return CONFIRM_REPEAT
+    else:
+        chosen_days.append(DAYS.index(message.text))
+
+        available_days = list(DAYS)
+        [available_days.remove(DAYS[i]) for i in chosen_days]
+        reply_keyboard = tools.build_menu(available_days, 3, footer_buttons=['Done'])
+
+        message.reply_text(GET_C_REP_MSG.format(', '.join([DAYS[i] for i in chosen_days])) + REMIND_CANCEL_MSG,
+                           reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, selective=True))
+        return CUSTOM_DAYS
+
+
+def is_repeat_good(bot, update, user_data):
+    """
+    ***----***
+    :param bot: the bot class
+    :type bot: telegram.Bot
+    :param update: the update message
+    :type update: telegram.Update
+    :param user_data: the user's data
+    :type user_data: dict
+    :return:
+    """
+    message = update.message  # type: telegram.Message
+    if message.text == "Yes!":
+        message.reply_text(END_MSG)
+        message.reply_text("user_data:\n" + str(user_data))
+        return ConversationHandler.END
+    else:
+        reply_keyboard = tools.build_menu(['Every day', 'Every month', 'Custom', 'Nope'], 3)
+
+        message.reply_text('Good!\n\nNow ' + GET_REP_MSG + REMIND_CANCEL_MSG,
+                           reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, selective=True))
+        return REPEAT
 
 
 def cancel(bot, update):
@@ -201,13 +359,25 @@ tools.add_conversations(ConversationHandler(
 
         CONFIRM_MESSAGE: [RegexHandler('^(Yes!|No!)$', is_message_good, pass_user_data=True)],
 
-        DATE: [RegexHandler('^(Today|Tomorrow|Further)$', select_date_type, pass_user_data=True)],
+        DATE: [RegexHandler('^(Today|Tomorrow|No, a different date)$', select_date_type, pass_user_data=True)],
 
         CUSTOM_DATE: [RegexHandler('^(0{0,1}[1-9]|[12][0-9]|3[01])\.(0{0,1}[1-9]|1[012])\.20\d\d$',
                                    handle_custom_date, pass_user_data=True),
                       RegexHandler('^([Ss]unday|[Mm]onday|[Tt]uesday|[Ww]ednesday|[Tt]hursday|[Ff]riday|[Ss]aturday)$',
-                                   handle_day_name, pass_user_data=True)
-                      ],
+                                   handle_day_name, pass_user_data=True)],
+
+        HOUR: [RegexHandler('^([01]{0,1}[0-9]|2[0-3]):[0-5]{0,1}[0-9]$', get_hour, pass_user_data=True)],
+
+        CONFIRM_TIME: [RegexHandler('^(Yes!|No!)$', is_time_good, pass_user_data=True)],
+
+        REPEAT: [RegexHandler('^(Every day|Every month|Custom|Nope)$', get_repeat_type, pass_user_data=True)],
+
+        CUSTOM_DAYS: [RegexHandler('^([Ss]unday|[Mm]onday|[Tt]uesday|[Ww]ednesday|[Tt]hursday|[Ff]riday|'
+                                   '[Ss]aturday|Done)$',
+                                   handle_custom_days, pass_user_data=True)],
+
+        CONFIRM_REPEAT: [RegexHandler('^(Yes!|No!)$', is_repeat_good, pass_user_data=True)],
+
     },
 
     fallbacks=[CommandHandler('cancel', cancel)]
